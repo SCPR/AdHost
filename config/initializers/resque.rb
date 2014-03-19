97e8e4f1491aa2f7a5401eaa58dbfd14ebdded26
Bryan Ricker <bricker88@gmail.com>
2014-03-18 23:59:30 -0700

    Fix AR reconnection on Resque fork

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,8 @@
-# Every time a job is started, make sure the connection
-# to MySQL is okay. This avoids the "MySQL server has gone away"
-# error.
-Resque.redis = "localhost:6379"
+Resque.redis = "localhost:6379/6"
 
-Resque.after_fork = Proc.new {
-  ActiveRecord::Base.verify_active_connections!
-}
+Resque.after_fork do |job|
+  # Every time a job is started, make sure the connection
+  # to MySQL is okay. This avoids the "MySQL server has gone away"
+  # error.
+  ActiveRecord::Base.connection_pool.connections.each(&:verify!)
+end
