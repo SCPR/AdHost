a225a007dc07a4ffb7dd2b3083ddac42cbe430f1
Bryan Ricker <bricker88@gmail.com>
2014-03-20 02:03:12 -0700

    Add build script for FFMpeg on circle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+if ENV['CIRCLECI']
+  FFMPEG.ffmpeg_binary = "/home/ubuntu/ffmpeg_build/ffmpeg/bin/ffmpeg"
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
